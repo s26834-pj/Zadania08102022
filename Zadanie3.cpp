6b0c96d6f811be3fbd74ded5e55c48d4526bc63c
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-10-18 12:46:19 +0200

    Add files via upload

---
+++ Zadanie3.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int liczba;
+   cout << "Wprowadz liczbe calkowita" << endl;
+   cin >> liczba;
+
+   if (liczba == 0)
+		{
+	    cout << "Liczba jest rowna 0" << endl;
+		}
+		else if (liczba % 2 == 0)
+			{
+			cout << "Liczba jest parzysta" << endl;
+			}
+			else
+				{
+				cout << "Liczba jest nieparzysta" << endl;
+				}
+   return 0;
+}
