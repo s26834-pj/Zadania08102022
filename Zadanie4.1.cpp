9181da9df9d7a182a620ee8a58084e70da976d41
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-10-18 13:22:23 +0200

    Add files via upload

---
+++ Zadanie4.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float liczba1, liczba2, liczba3;
+    cout << "Podaj trzy liczby" << endl;
+    cin >> liczba1;
+    cin >> liczba2;
+    cin >> liczba3;
+
+    if ((liczba1 > liczba2) && (liczba1 > liczba3))
+            {
+        cout << "Liczba nr 1 o wartosci: " << liczba1 << " jest najwieksza" << endl;
+            }
+            else if ((liczba2 > liczba1) && (liczba2 > liczba3))
+                    {
+                    cout<< "Liczba nr 2 o wartosci: " << liczba2 << " jest najwieksza" << endl;
+                    }
+                    else
+                        { 
+                         cout<< "Liczba nr 3 o wartosci: " << liczba3 << " jest najwieksza" << endl;
+                        }
+
+    return 0;
+}
