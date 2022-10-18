1e3463d824f3bbfb036f35746300e6f58148fc75
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-10-18 12:27:28 +0200

    Add files via upload

---
+++ Zadanie2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int liczba;
+
+    cout << "Wprowadz liczbe calkowita" << endl;
+    cin >> liczba;
+
+    if (liczba == 0) 
+        {
+        cout << "Liczba jest rowna 0" << endl;
+        }
+        else if (liczba > 0) 
+            {
+            cout << "Liczba jest wieksza od 0" << endl;
+            }
+             else 
+                {
+                 cout << "Liczba jest mniejsza od 0" << endl;
+                }
+    
+    return 0;
+}
